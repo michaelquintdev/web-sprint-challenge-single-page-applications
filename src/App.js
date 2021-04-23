b061bfce1b4b2a5389c0683aaacc68ae9b36a36a
michaelquintdev <michaelquintdev@gmail.com>
2021-04-23 15:05:09 -0500

    Done with SIMPLE form. Need to add disabled property still.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import {Route, Link, Switch} from 'react-router-dom';
 import Form from './components/Form.js';
 import Home from './components/Home.js';
 
+
+// Setting initial form Values
+const initialFormValues = {
+    name: '',
+    size: '',
+    topping1: false,
+    topping2: false,
+    topping3: false,
+    topping4: false,
+    special: '',
+}
+
 const App = () => {
+  const [pizzas, setPizzas] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]: inputValue})
+  }
+  const submitForm = () => {
+    const newPizza = {
+      name: formValues.name.trim(),
+      size: formValues.size.trim(),
+      special: formValues.special.trim(),
+    }
+    setPizzas([...pizzas, newPizza]);
+    setFormValues(initialFormValues);
+  }
+
   return (
     <div className = 'homepage'>
       <div className = 'nav-links'>
@@ -14,7 +42,7 @@ const App = () => {
     {/* Switch that will send you to different components */}
     <Switch>
       <Route path = '/pizza'>
-        <Form id = 'pizza-form'/>
+        <Form id = 'pizza-form' values = {formValues} update = {updateForm} submit = {submitForm}/>
       </Route>
       <Route path = '/'>
         <Home />
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,99 @@
 import React from 'react';
 
-export default function Form(){
+export default function Form(props){
+    const {values, update, submit} = props;
+
+    const onChange = evt => {
+        const { name, value } = evt.target;
+        update(name, value);
+    }
+    const onSubmit = evt => {
+        evt.preventDefault();
+        submit();
+    }
+
     return (
-    <div>I like pizza</div>
+        // Constructing the form
+        <form id = 'pizza-form' onSubmit={onSubmit}>
+            <div className = 'form-container'>
+                <h2>Form to get Lambda Pizza!</h2>
+                <label>
+                    Name
+                    <input 
+                        id = 'name-input'
+                        value = {values.name}
+                        onChange = {onChange}
+                        name = 'name'
+                        type = 'text'
+                    />
+                </label>
+                <label>
+                    Size of Pizza:
+                    <select name = 'size' value = {values.size} onChange = {onChange}>
+                        <option value = ''>Select a size</option>
+                        <option value = 'Small'>Small</option>
+                        <option value = 'Medium'>Medium</option>
+                        <option value = 'Large'>Large</option>
+                    </select>
+                </label>
+                <div className = 'checkboxes'>
+                    <h3>Toppings</h3>
+                    <label>
+                        Pineapple
+                        <input
+                            checked = {null}
+                            onChange = {onChange}
+                            name = 'pineapple'
+                            type = 'checkbox'
+                        />
+                    </label>
+                    <label>
+                        <br></br>   
+                        Solid Concrete
+                        <input 
+                            checked = {null}
+                            onChange = {onChange}
+                            name = 'concrete'
+                            type = 'checkbox'
+                        />
+                    </label>
+                    <label>
+                        <br></br>
+                        Drywall
+                        <input
+                            checked = {null}
+                            onChange = {onChange}
+                            name = 'drywall'
+                            type = 'checkbox'
+                        />
+                    </label>
+                    <label>
+                        <br></br>
+                        Straight Hydrochloric Acid
+                        <input
+                            checked = {null}
+                            onChange = {onChange}
+                            name = 'acid'
+                            type = 'checkbox'
+                        />
+                    </label>
+                    <label>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        Any special Instructions for delivery?
+                        <br></br>
+                    <input 
+                        id = 'special-text'
+                        value = {values.special}
+                        onChange = {onChange}
+                        name = 'special'
+                        type = 'text'
+                    />
+                </label>
+                </div>
+            </div>
+            <button id = 'order-button'>Submit Order Here</button>
+        </form>
     );
 }
